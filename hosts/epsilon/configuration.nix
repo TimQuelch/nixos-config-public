80f4a93f495acee575cf67c6f46e49aadb42198f
Tim Quelch <tim@tquelch.com>
2024-09-21 16:50:57 +1000

    Add steam

--- hosts/epsilon/configuration.nix
+++ hosts/epsilon/configuration.nix
@@ -11,6 +11,11 @@
   networking.useDHCP = true;
   networking.wireless.userControlled.enable = true;
 
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+  };
+
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.05";
 }
