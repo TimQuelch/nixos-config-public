fca1c75559e3b8b27f644494cb3a5a584c04df6f
Tim Quelch <tim@tquelch.com>
2024-09-22 16:53:17 +1000

    Increase number of workspaces

--- modules/home/hyprland.nix
+++ modules/home/hyprland.nix
@@ -1,5 +1,7 @@
 { pkgs, config, ... }:
-{
+let
+  nWorkspaces = 10;
+in {
   home.packages = with pkgs; [
     firefox
     wofi
@@ -38,15 +40,15 @@
       builtins.concatLists (builtins.genList (
         x: let
           ws = let
-            c = (x + 1) / 5;
+            c = (x + 1) / nWorkspaces;
           in
-            builtins.toString (x + 1 - (c * 5));
+            builtins.toString (x + 1 - (c * nWorkspaces));
         in [
           "$mod, ${ws}, workspace, ${toString (x + 1)}"
           "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
         ]
       )
-      5)
+      nWorkspaces)
     );
     bindm = [
       # LMB to move, RMB to resize
