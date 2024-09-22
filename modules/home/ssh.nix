2137c867ab4f06e324bcc97af1c130d7066afc6f
Tim Quelch <tim@tquelch.com>
2024-09-22 18:24:42 +1000

    Automatically add ssh key

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -45,5 +45,28 @@ in {
     );
 
     services.ssh-agent.enable = true;
+
+    # Add the primary SSH key to the authorised keys file if it is not already there. Note that this
+    # is somewhat impure because it will not remove the key if the key changes. An alternate
+    # solution would be to link the authorized keys file to a file in the nix store, however this
+    # would remove the ability to edit this file easily
+    systemd.user.services.add-authorised-ssh-key = {
+      Unit.Description = "Add primary ssh key to authorised_keys file";
+      Install = {
+        WantedBy = [ "sops-nix.service" ];
+        After = [ "sops-nix.service" ];
+      };
+      Service = let
+        keyPath = config.sops.secrets."ssh_auth_keys/primary.pub".path;
+        authorizedKeysPath = "${config.home.homeDirectory}/.ssh/authorized_keys";
+      in {
+        Type = "oneshot";
+        ExecStart = ''
+          ${pkgs.bash}/bin/bash -c \
+            "${pkgs.gnugrep}/bin/grep -q -f ${keyPath} ${authorizedKeysPath} \
+             || cat ${keyPath} >> ${authorizedKeysPath}"
+        '';
+      };
+    };
   };
 }
