12dd2df75ad3438cbd29ab368ff18595629dbeed
Tim Quelch <tim@tquelch.com>
2024-10-02 00:53:55 +1000

    Enable swap

--- hardware/desktop-hardware-configuration.nix
+++ hardware/desktop-hardware-configuration.nix
@@ -39,7 +39,9 @@
       options = [ "subvol=@nix" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    { device = "/swap/swapfile"; }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
