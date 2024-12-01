8ca105fa6dcec121e3cf9ac361d0545652f87976
Tim Quelch <tim@tquelch.com>
2024-12-01 20:00:33 +1100

    Update public repo script

--- scripts/prepare-public-repo.sh
+++ scripts/prepare-public-repo.sh
@@ -6,7 +6,15 @@ printf "Cloning repo to temp dir...\n"
 dir=$(mktemp -d)
 git clone --no-local . "$dir"
 
-printf "\n\nFiltering repo omitting private data...\n"
+cleanup() {
+    if [[ -n "${dir:-}" ]] && [[ -d "$dir" ]]; then
+        printf "\nCleaning up temporary directory...\n"
+        rm -rf "$dir"
+    fi
+}
+trap cleanup EXIT ERR INT TERM
+
+printf "\nFiltering repo omitting private data...\n"
 cd "$dir"
 git filter-repo \
     --invert-paths \
@@ -15,7 +23,39 @@ git filter-repo \
     --path hosts/work-laptop \
     --path modules/home/work
 
-printf "\n\nPopulating private facing stubs...\n"
+printf "\nPopulating private facing stubs...\n"
 cp -r ./scripts/public-stubs/. .
 git add -A
 git commit -m "Populating redacted public facing stubs"
+
+printf "\nPulling exsisting public repo...\n"
+git remote add public git@github.com:TimQuelch/nixos-config-public
+git fetch public
+
+printf "\nRebasing onto public master branch...\n"
+git config advice.skippedCherryPicks false
+git rebase --verbose public/master
+
+# Count new commits
+NEW_COMMITS=$(git log --pretty=format:"%h" "public/master"..HEAD | wc -l)
+
+# Check if there are any new commits
+if [[ "$NEW_COMMITS" -eq 0 ]]; then
+    printf "\nNo new commits to push. Exiting.\n"
+    exit 0
+fi
+
+printf "\n\e[1;34mNew commits to be pushed:\e[0m\n"
+git log --pretty=format:"%C(yellow)%h%Creset %s" "public/master"..HEAD
+
+read -r -t 30 -p $'\e[1;33mDo you want to push these changes to the public repository? (y/N): \e[0m' confirm || confirm="n"
+
+confirm=$(echo "${confirm:-n}" | tr '[:upper:]' '[:lower:]' | xargs)
+
+if [[ "$confirm" == "y" || "$confirm" == "yes" ]]; then
+    printf "\nPushing to public repository...\n"
+    git push --force-with-lease public HEAD:master
+    printf "\nPush completed successfully.\n"
+else
+    printf "\nPush cancelled.\n"
+fi
