bf1b272d9ee8f6de54540ef52325e7ae9d2b4123
Tim Quelch <tim@tquelch.com>
2024-12-11 20:18:03 +1100

    Disable hypridle because suspend isn't working

--- hosts/alpha/home.nix
+++ hosts/alpha/home.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, system, ... }: {
+{ lib, config, pkgs, system, ... }: {
   modules.gui.enable = true;
   modules.email.enable = true;
+
+  services.hypridle.enable = lib.mkForce false;
 }
