148152b32cde41fa69a9397ac1ba007920256e7a
Tim Quelch <tim@tquelch.com>
2025-01-13 23:09:03 +1100

    Add ghostty

--- modules/home/gui/default.nix
+++ modules/home/gui/default.nix
@@ -1,12 +1,13 @@
 { lib, config, options, ... }:
 let cfg = config.modules.gui;
 in {
-  imports = [ ./hyprland.nix ./packages.nix ];
+  imports = [ ./hyprland.nix ./packages.nix ./ghostty.nix ];
 
   options.modules.gui.enable = lib.mkEnableOption "gui configuration";
 
   config = lib.mkIf cfg.enable {
     modules.gui.hyprland.enable = true;
     modules.gui.packages.enable = true;
+    modules.gui.ghostty.enable = true;
   };
 }
