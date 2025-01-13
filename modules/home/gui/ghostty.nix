f7f6f248518179c2fd7917da66cc17b0903d6614
Tim Quelch <tim@tquelch.com>
2025-01-13 23:09:03 +1100

    Add ghostty

---
+++ modules/home/gui/ghostty.nix
@@ -0,0 +1,13 @@
+{ options, config, lib, pkgs, ... }:
+let cfg = config.modules.gui.packages;
+in {
+  options.modules.gui.ghostty.enable = lib.mkEnableOption "ghostty";
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [ pkgs.ghostty ];
+
+    xdg.configFile."ghostty/config".text = ''
+      window-decoration=false
+    '';
+  };
+}
