8ea88be09b1ddbbb4bd0d92f3c41b76aa46f2669
Tim Quelch <tim@tquelch.com>
2025-02-02 21:40:22 +1100

    Add btrfs configuration for desktop

--- hardware/desktop-hardware-configuration.nix
+++ hardware/desktop-hardware-configuration.nix
@@ -32,6 +32,11 @@
     };
   };
 
+  modules.btrfs-maintenance = {
+    enable = true;
+    mountPoints = [ "/" ];
+  };
+
   swapDevices = [{ device = "/swap/swapfile"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
