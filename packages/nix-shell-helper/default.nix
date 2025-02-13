15d073e0464a38cac018ec7cfbea7ab8552161b2
Tim Quelch <timothy.quelch@versent.com.au>
2025-02-13 13:32:19 +1100

    Add helper script for nix shell

---
+++ packages/nix-shell-helper/default.nix
@@ -0,0 +1,23 @@
+{ writeShellApplication, registry ? "nixpkgs" }:
+writeShellApplication {
+  name = "ns";
+  text = ''
+    if [ $# -eq 0 ]; then
+      echo "Usage: ns package1 [package2 ...]"
+      exit 1
+    fi
+
+    declare -a packages=()
+    declare -a options=()
+
+    for arg in "$@"; do
+      if [[ "$arg" == --* ]]; then
+        options+=("$arg")
+      else
+        packages+=("${registry}#$arg")
+      fi
+    done
+
+    exec nix shell "''${packages[@]}" "''${options[@]}"
+  '';
+}
