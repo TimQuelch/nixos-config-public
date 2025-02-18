b02546573fcd3240c60e081474cb60d83798db15
Tim Quelch <tim@tquelch.com>
2025-02-18 21:52:41 +1100

    Fix cache config

--- modules/os/nix-cache.nix
+++ modules/os/nix-cache.nix
@@ -26,13 +26,27 @@ in
 
       [servers.theta]
       endpoint = "https://nix.theta.tquelch.com"
-      token_file = "${config.sops.secrets.nix-cache-jwt.path}"
+      token-file = "${config.sops.secrets.nix-cache-jwt.path}"
     '';
 
     nix.settings = {
-      substituters = [ "https://nix.theta.tquelch.com/attic/theta" ];
+      substituters = [ "https://nix.theta.tquelch.com/theta" ];
       trusted-public-keys = [ "theta:bEYWsk8RIf6epx3nKxVjn+hWWOFyX6rlOqBLqBUBTOw=" ];
       netrc-file = config.sops.templates.nix-cache-netrc.path;
+    };
+
+    systemd.services.attic-nix-cache-upload = {
+      environment.XDG_CONFIG_HOME = "/etc";
+      script = "${lib.getExe pkgs.attic-client} watch-store theta";
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "network-online.target" ];
+      serviceConfig = {
+        Nice = 19;
+        IOSchedulingClass = "idle";
+        IOSchedulingPriority = 7;
+        CPUSchedulingPolicy = "idle";
+        CPUWeight = 10;
+      };
     };
   };
 }
