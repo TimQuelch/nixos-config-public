cc63e8ab0aaefb3d85f301b78bb0e222f88cef78
Tim Quelch <tim@tquelch.com>
2025-04-04 20:23:30 +1100

    Add timers for email

--- modules/home/email.nix
+++ modules/home/email.nix
@@ -17,6 +17,7 @@ in
   config = mkIf cfg.enable {
     sops.secrets.fastmail_token = { };
 
+    accounts.email.maildirBasePath = "maildir";
     accounts.email.accounts.personal = {
       primary = true;
       address = "tim@tquelch.com";
@@ -29,5 +30,23 @@ in
 
     programs.notmuch.enable = true;
     programs.mujmap.enable = true;
+
+    systemd.user.services.mujmap-sync = {
+      Unit.Description = "Sync maildir with mujmap";
+      Service = {
+        Type = "exec";
+        WorkingDirectory = "${config.accounts.email.maildirBasePath}/personal";
+        ExecStart = "${lib.getExe config.programs.mujmap.package} sync -v";
+      };
+    };
+
+    systemd.user.timers.mujmap-sync = {
+      Unit.Description = "Sync maildir with mujmap";
+      Timer = {
+        OnBootSec = "3min";
+        OnUnitActiveSec = "30min";
+      };
+      Install.WantedBy = [ "timers.target" ];
+    };
   };
 }
