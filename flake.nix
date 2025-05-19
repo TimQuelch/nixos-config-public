4175d94b6d056022a3b44c51ffc3ca9faad9168f
Tim Quelch <tim@tquelch.com>
2025-05-19 11:16:28 +1000

    Switch home-manager to stable 25.05 release

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     wsl.url = "github:nix-community/nixos-wsl";
     wsl.inputs.nixpkgs.follows = "nixpkgs";
